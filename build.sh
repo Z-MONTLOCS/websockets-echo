29093450bd555fb0957c431797d4b29618dd0b56
z-montlocs <agw.developer@gmail.com>
2023-08-25 06:44:11 -0500

    first commit

--- build.sh
+++ build.sh
@@ -1,13 +1,3 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-poetry install
-
-python manage.py collectstatic --no-input
-python manage.py migrate
-
-
 #!/bin/bash
 
 echo "...Installing packages..."
